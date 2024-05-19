d82d1f9e7b2992ad43eb7685938f4b8b8e3d30de
IsraelDev560 <devisrael560@gmail.com>
2024-05-19 01:14:05 -0300

    Desafios List

---
+++ collections-java-api-2024/src/CarrinhoDeCompras/Item.java
@@ -0,0 +1,33 @@
+package CarrinhoDeCompras;
+
+public class Item {
+    private String item;
+    private double preco;
+    private int quantidade;
+
+    public Item(String item, double preco, int quantidade) {
+        this.item = item;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public String getItem(){
+        return item;
+    }
+
+    public double getPreco(){
+        return preco;
+    }
+    public int getQuantidade(){
+        return quantidade;
+    }
+
+    @Override
+    public String toString() {
+        return "Lista de Compras: " +
+                "Item = '" + item + '\'' +
+                ", preço = " + preco +
+                ", quantidade = " + quantidade ;
+
+    }
+}
