9613cc04d3f890cec268a1c6c95fb2daf5fbdaa5
IsraelDev560 <devisrael560@gmail.com>
2024-07-09 23:11:40 -0300

    add novas features

--- santander-dev-week/src/main/java/me/dio/model/BaseInfo.java
+++ santander-dev-week/src/main/java/me/dio/model/BaseInfo.java
@@ -23,6 +23,13 @@ public abstract class BaseInfo {
         this.description = description;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getIcon() {
         return icon;
